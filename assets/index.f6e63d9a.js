7fd123d10c7b41330e01c99fc84f189dbe954945
Na Hyejin <randall09@vers.kr>
2022-06-09 16:05:36 +0900

    Updates

--- assets/index.f6e63d9a.js
+++ assets/index.f6e63d9a.js
@@ -52,7 +52,7 @@ Error generating stack: `+o.message+`
   background-color: var(--nav-color);
   display: flex;
   justify-content: space-between;
-  box-shadow: inset 0 0 0 3px var(--border-color);
+  border: var(--border);
   &:first-of-type {
     top: 0;
     border-radius: 20px 20px 0 0;
@@ -67,12 +67,33 @@ Error generating stack: `+o.message+`
   width: 336px;
   height: 64px;
   > div {
-    background-color: var(--light-purple);
+    background-color: var(--light-color);
     width: 336px;
     height: 27px;
     border-radius: 14px;
-    border: 3px solid var(--border-color);
+    border: var(--border);
     overflow: hidden;
+    position: relative;
+    &::after {
+      display: block;
+      content: '';
+      width: 100%;
+      height: 100%;
+      border-radius: 14px;
+      position: absolute;
+      z-index: 10;
+      top: 0;
+      left: 0;
+      box-shadow: inset 1px 1px 1px var(--action-btn),
+        //top highlight
+        0 0 0 4px var(--color-overlay-dark),
+        //outer shadow
+        inset -1px -1px 1px var(--color-bg-dark),
+        //bottom lowlight
+        inset 0rem 0.5rem 0.1rem var(--light-color),
+        //inset shadow
+        inset 0.5rem 0.5rem 0.5rem var(--light-color); //button gloss
+    }
     &:first-of-type {
       margin-bottom: 10px;
       > div {
@@ -92,15 +113,36 @@ Error generating stack: `+o.message+`
 `,Go=Ir.button`
   width: 74px;
   height: 27px;
-  background-color: var(--action-btn);
-  box-shadow: inset 0 0 0 1px var(--border-color);
+  background-color: var(--other-btn);
+  border: var(--border);
   border-radius: 8px;
   position: relative;
   color: var(--icon-color);
+  box-shadow: -1px -1px 1px var(--other-btn),
+    //top highlight
+    0 0 0 4px var(--color-overlay-medium),
+    //outer shadow
+    1px 1px 1px var(--color-bg-dark),
+    //bottom lowlight
+    inset 0 0 0 var(--color-overlay-dark),
+    //inset shadow
+    inset 0.5rem 0.5rem 0.25rem var(--light-color); //button gloss
+  transition: all 0.2s;
   > svg {
     width: 20px;
     height: 20px;
     fill: var(--icon-color);
+  }
+  &:active {
+    box-shadow: inset 1px 1px 1px var(--action-btn),
+      //top highlight
+      0 0 0 4px var(--color-overlay-dark),
+      //outer shadow
+      inset -1px -1px 1px var(--color-bg-light),
+      //bottom lowlight
+      inset 0.5rem 0.5rem 0.75rem var(--color-bg-dark),
+      //inset shadow
+      inset 0.5rem 0.5rem 0.5rem var(--light-color); //button gloss
   }
 `,t0=Ir.div`
   z-index: 20;
@@ -113,7 +155,18 @@ Error generating stack: `+o.message+`
     border-radius: 40px;
     background-color: var(--action-btn);
     position: absolute;
-    box-shadow: inset 0 0 0 1px var(--border-color);
+    outline: none;
+    border: var(--border);
+    box-shadow: -1px -1px 1px var(--action-btn),
+      //top highlight
+      0 0 0 4px var(--color-overlay-medium),
+      //outer shadow
+      1px 1px 1px var(--color-bg-dark),
+      //bottom lowlight
+      inset 0 0 0 var(--color-overlay-dark),
+      //inset shadow
+      inset 0.5rem 0.5rem 0.25rem var(--light-color); //button gloss
+    transition: all 0.2s;
     svg {
       width: 30px;
       height: 30px;
@@ -121,7 +174,7 @@ Error generating stack: `+o.message+`
       /* stroke-width: 10;
         stroke: var(--border-color); */
     }
-    &::before {
+    /* &::before {
       width: 64px;
       height: 64px;
       display: block;
@@ -131,15 +184,18 @@ Error generating stack: `+o.message+`
       left: 2px;
       border-radius: 40px;
       z-index: -1;
-      background-color: var(--action-btn-before);
-    }
+      background-color: var(--action-btn-shadow);
+    } */
     &:active {
-      top: 2px;
-      left: 2px;
-      &::before {
-        top: 0;
-        left: 0;
-      }
+      box-shadow: inset 1px 1px 1px var(--action-btn),
+        //top highlight
+        0 0 0 4px var(--color-overlay-dark),
+        //outer shadow
+        inset -1px -1px 1px var(--color-bg-light),
+        //bottom lowlight
+        inset 0.5rem 0.5rem 0.75rem var(--color-bg-dark),
+        //inset shadow
+        inset 0.5rem 0.5rem 0.5rem var(--light-color); //button gloss
     }
   }
 `;var Qu={exports:{}},go={};/**
